21e112ca12ce68cdd59e32232c2ab4f32b190129
Leonid <scorpion23997@gmail.com>
2023-12-16 22:52:05 +0200

    formating code style
    remove unused namespaces

--- src/Controller/Api/v1/BookController.php
+++ src/Controller/Api/v1/BookController.php
@@ -11,7 +11,6 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
